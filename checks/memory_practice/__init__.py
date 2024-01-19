39073ef6a4e54e3c7d3f784a07fbafa7ebb1a4e6
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-19 16:13:46 -0600

    Update __init__.py

--- checks/memory_practice/__init__.py
+++ checks/memory_practice/__init__.py
@@ -2,16 +2,4 @@ import check50
 
 @check50.check()
 def memeory_leaks():
-    output = check50.run("valgrind ./memory_practice").stdout()
-    fail = False
-    message = ""
-    if "LEAK SUMMARY:" in output:
-        message += "Memory leak detected. "
-        fail = True
-    if "Invalid write of" in output:
-        message += "Invalid write detected."
-        fail = True
-    if fail:
-        raise check50.Failure(message)
-    else:
-        raise check50.Failure(output)
+    check50.c.valgrind("./memory_practice").stdout()
