c2f1e97a136814ea099b58ba6dead0673769711c
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 11:50:46 -0600

    Update __init__.py

--- checks/dmap/array/__init__.py
+++ checks/dmap/array/__init__.py
@@ -1,1 +1,27 @@
+import check50
+import check50.c
 
+@check50.check()
+def compiles():
+  """array.c compiles"""
+  check50.run("make array").exit(0)
+
+@check50.check()
+def output1():
+  """Output is correct"""
+  check50.run("./array").stdin(3).stdin(42).stdin(13).stdin(3).stdout("The sum is 58", regex=True).exit(0)
+
+@check50.check()
+def output2():
+  """Output is correct"""
+  check50.run("./array").stdin(2).stdin(11).stdin(9).stdout("The sum is 20", regex=True).exit(0)
+
+@check50.check()
+def output3():
+  """Output is correct"""
+  check50.run("./array").stdin(3).stdin(1).stdin(2).stdin(-3).stdout("The sum is 0", regex=True).exit(0)
+
+@check50.check()
+def memeory_leaks():
+  """No memory leaks detected"""
+  check50.c.valgrind("./intptr").stdin(3).stdin(42).stdin(13).stdin(3).stdout("The sum is 58", regex=True).exit(0)
