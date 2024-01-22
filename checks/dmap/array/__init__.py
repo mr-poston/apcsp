4809e6938c523e90efbf2dcb312b8eec418f1c8f
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 11:54:09 -0600

    Update __init__.py

--- checks/dmap/array/__init__.py
+++ checks/dmap/array/__init__.py
@@ -9,19 +9,19 @@ def compiles():
 @check50.check()
 def output1():
   """Output is correct"""
-  check50.run("./array").stdin(3).stdin(42).stdin(13).stdin(3).stdout("The sum is 58", regex=True).exit(0)
+  check50.run("./array").stdin("3").stdin("42").stdin("13").stdin("3").stdout("The sum is 58", regex=True).exit(0)
 
 @check50.check()
 def output2():
   """Output is correct"""
-  check50.run("./array").stdin(2).stdin(11).stdin(9).stdout("The sum is 20", regex=True).exit(0)
+  check50.run("./array").stdin("2").stdin("11").stdin("9").stdout("The sum is 20", regex=True).exit(0)
 
 @check50.check()
 def output3():
   """Output is correct"""
-  check50.run("./array").stdin(3).stdin(1).stdin(2).stdin(-3).stdout("The sum is 0", regex=True).exit(0)
+  check50.run("./array").stdin("3").stdin("1").stdin("2").stdin("-3").stdout("The sum is 0", regex=True).exit(0)
 
 @check50.check()
 def memeory_leaks():
   """No memory leaks detected"""
-  check50.c.valgrind("./intptr").stdin(3).stdin(42).stdin(13).stdin(3).stdout("The sum is 58", regex=True).exit(0)
+  check50.c.valgrind("./intptr").stdin("1").stdout("The sum is 42", regex=True).exit(0)
