7b1e685d3bbdb515a452bacaabbef84efab238ce
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 12:22:47 -0600

    Update __init__.py

--- checks/dmap/concatenate/__init__.py
+++ checks/dmap/concatenate/__init__.py
@@ -1,1 +1,27 @@
+import check50
+import check50.c
 
+@check50.check()
+def compiles():
+  """concatenate.c compiles"""
+  check50.run("make concatenate").exit(0)
+
+@check50.check()
+def output1():
+  """Output is correct"""
+  check50.run("./concatenate").stdin("black cat").stdout("blackcat", regex=True).exit(0)
+
+@check50.check()
+def output2():
+  """Output is correct"""
+  check50.run("./concatenate").stdin("home run").stdout("homerun", regex=True).exit(0)
+
+@check50.check()
+def output3():
+  """Output is correct"""
+  check50.run("./concatenate").stdin("super charger").stdout("supercharger", regex=True).exit(0)
+
+@check50.check()
+def memeory_leaks():
+  """No memory leaks detected"""
+  check50.c.valgrind("./concatenate").stdin("a b").stdout("ab", regex=True).exit(0)
