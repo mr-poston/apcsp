02d378878d6fbcab1beae6492ea3054d78a8725c
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 10:58:44 -0600

    Update __init__.py

--- checks/dmap/hi/__init__.py
+++ checks/dmap/hi/__init__.py
@@ -2,4 +2,15 @@ import check50
 
 @check50.check()
 def compiles():
+  """hi.c compiles"""
   check50.run("make hi").exit(0)
+
+@check50.check()
+def output():
+  """Output is correct"""
+  check50.run("./hi").stdout("HI!").exit(0)
+
+@check50.check()
+def memeory_leaks():
+  """No memory leaks detected"""
+    check50.c.valgrind("./hi").stdout("HI!").exit(0)
