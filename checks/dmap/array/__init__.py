2e11dc6e2c71d0a84e75cbaf77b03fe66de9f49d
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 12:10:37 -0600

    Update __init__.py

--- checks/dmap/array/__init__.py
+++ checks/dmap/array/__init__.py
@@ -14,14 +14,14 @@ def output1():
 @check50.check()
 def output2():
   """Output is correct"""
-  check50.run("./array").stdout("The sum is 20", regex=True).exit(0)
+  check50.run("./array").stdin("2 11 9").stdout("The sum is 20", regex=True).exit(0)
 
 @check50.check()
 def output3():
   """Output is correct"""
-  check50.run("./array").stdin("3").stdin("1").stdin("2").stdin("-3").stdout("The sum is 0", regex=True).exit(0)
+  check50.run("./array").stdin("3 1 2 -3").stdout("The sum is 0", regex=True).exit(0)
 
 @check50.check()
 def memeory_leaks():
   """No memory leaks detected"""
-  check50.c.valgrind("./array").stdin("1").stdout("The sum is 42", regex=True).exit(0)
+  check50.c.valgrind("./array").stdin("1 42").stdout("The sum is 42", regex=True).exit(0)
