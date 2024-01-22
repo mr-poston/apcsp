20bf9e91e2b72d7c62e262891e45394021df0954
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 12:02:01 -0600

    Update __init__.py

--- checks/dmap/array/__init__.py
+++ checks/dmap/array/__init__.py
@@ -9,19 +9,19 @@ def compiles():
 @check50.check()
 def output1():
   """Output is correct"""
-  check50.run("./array").stdin("3", prompt=False).stdin("42", prompt=False).stdin("13", prompt=False).stdin("3", prompt=False).stdout("The sum is 58", regex=True).exit(0)
+  check50.run("./array").stdin("3").stdin("42").stdin("13").stdin("3").stdout("The sum is 58", regex=True).exit(0)
 
 @check50.check()
 def output2():
   """Output is correct"""
-  check50.run("./array").stdin("2", prompt=False).stdin("11", prompt=False).stdin("9", prompt=False).stdout("The sum is 20", regex=True).exit(0)
+  check50.run("./array").stdin("2").stdin("11").stdin("9").stdout("The sum is 20", regex=True).exit(0)
 
 @check50.check()
 def output3():
   """Output is correct"""
-  check50.run("./array").stdin("3", prompt=False).stdin("1", prompt=False).stdin("2", prompt=False).stdin("-3", prompt=False).stdout("The sum is 0", regex=True).exit(0)
+  check50.run("./array").stdin("3").stdin("1").stdin("2").stdin("-3").stdout("The sum is 0", regex=True).exit(0)
 
 @check50.check()
 def memeory_leaks():
   """No memory leaks detected"""
-  check50.c.valgrind("./array").stdin("1", prompt=False).stdout("The sum is 42", regex=True).exit(0)
+  check50.c.valgrind("./array").stdin("1").stdout("The sum is 42", regex=True).exit(0)
