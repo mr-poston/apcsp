cf482e30241e53de08ef0760d6909135bf60791a
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-22 11:20:29 -0600

    Update __init__.py

--- checks/dmap/intptr/__init__.py
+++ checks/dmap/intptr/__init__.py
@@ -1,1 +1,17 @@
+import check50
+import check50.c
 
+@check50.check()
+def compiles():
+  """intptr.c compiles"""
+  check50.run("make intptr").exit(0)
+
+@check50.check()
+def output():
+  """Output is correct"""
+  check50.run("./intptr").stdout("42 is at 0x.*", regex=True).exit(0)
+
+@check50.check()
+def memeory_leaks():
+  """No memory leaks detected"""
+  check50.c.valgrind("./intptr").stdout("42 is at 0x.*", regex=True).exit(0)
